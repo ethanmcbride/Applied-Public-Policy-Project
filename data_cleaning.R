1621cce70a3246a9ddc0cc1a5755b365e1d5d000
Ethan McBride <em4742a@american.edu>
2025-07-08 12:24:04 -0400

    Actively working to join data sets

--- data_cleaning.R
+++ data_cleaning.R
@@ -4,9 +4,10 @@ library(janitor)
 
 # Load the data
 
+### Data from the US Census ###
 census_data_PA <- read_csv("data/cc-est2024-alldata-42.csv")
 
-census_data_cleaned_v2 <- census_data_PA |>
+census_data_cleaned <- census_data_PA |>
   mutate(
     YEAR = case_when(
       YEAR == 1 ~ as.Date("2020-04-01"),
@@ -43,11 +44,30 @@ census_data_cleaned_v2 <- census_data_PA |>
   clean_names() |>
   rename(date = year,
          age_group = agegrp) |>
-  select(-c(state, sumlev, stname))
+  select(-c(state, sumlev, stname)) |>
+  filter(date != "2020-04-01")
 
+### Data from the US Department of Agriculture ###
 poverty_data <- read_xlsx("data/PovertyAreaMeasures2023.xlsx", sheet = "COUNTY PUBLIC DATA")
 
 poverty_data_cleaned <- poverty_data |>
   clean_names() |>
-  filter(stusab == "PA")
+  filter(stusab == "PA") |>
+  mutate(county_name = str_remove(county_name, ", Pennsylvania")) |>
+  select(-c(stusab, fips)) |>
 
+### Medicaid data ###
+medicaid_data <- read_csv("data/Rate_of_Women_on_Medical_Assistance__MA__Diagnosed_with_Opioid_Use_Disorder__OUD__during_Pregnancy_CY_2016-Current_Statewide_Department_of_Human_Services__DHS__20250624.csv")
+
+medicaid_data_cleaned <- medicaid_data |>
+  clean_names() |>
+  filter(geographic_name != "Commonwealth") |>
+  filter(grepl("Annual", time_period)) |>
+  mutate(year = as.Date(paste0(year, "-07-01")))
+
+### Join data sets ###
+joined_data <- medicaid_data_cleaned |>
+  left_join(census_data_cleaned, by = c("geographic_name" = "ctyname"), relationship = "many-to-many") |>
+  left_join(poverty_data_cleaned, by = c("geographic_name" = "county_name"), relationship = "many-to-many") |>
+  select(!geographic_area)
+            
